c0e0716e6fda090f043858d52a4c91069c8cd16a
Sartkis_13 <petrosyants.s@yandex.ru>
2025-05-04 08:10:55 +0300

    Ошибка с SendMessageAsync

---
+++ Bot.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Tbot;
+
+public class Bot : BackgroundService
+{
+    private ITelegramBotClient _telegramClient;
+
+    public Bot(ITelegramBotClient telegramClient)
+    {
+        _telegramClient = telegramClient;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _telegramClient.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            new ReceiverOptions() { AllowedUpdates = { } },
+            // Здесь выбираем, какие обновления хотим получать В данном случае разрешены все
+            cancellationToken: stoppingToken);
+
+        Console.WriteLine("Бот запущен");
+
+
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        //  Обрабатываем нажатия на кнопки  из Telegram Bot API: https://core.telegram.org/bots/api#callbackquery
+        if (update.Type == UpdateType.CallbackQuery)
+        {
+            await
+                _telegramClient.SendMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку", cancellationToken: cancellationToken);
+            return;
+        }
+        if (update.Type == UpdateType.Message)
+        {
+            await
+                _telegramClient.SendMessageAsync(update.Message.Chat.Id, "Вы отпаравили сообщение", cancellationToken: cancellationToken);
+            return;
+        }
+    }
+
+    public Task HandleErrorAsync(ITelegramBotClient botClietnt, Exception exception, CancellationToken cancellationToken)
+    {
+        //Задаем сообщение об ошибке в зависимости от того, какая именно ошибка произошла
+
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}] \n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        //выводим в консоль сообщение об ошибке
+        Console.WriteLine(errorMessage);
+
+        //Задержка перед повторным подключением
+        Console.WriteLine("Ожидаем 10 секунд перед повторным подключением");
+        Thread.Sleep(10000);
+        return Task.CompletedTask;
+    }
+}
+
